5dd282f3a21cecc85ce47831a04f0a1e121a26b6
Егор Зеленский <i@kha0s.ru>
2014-06-09 15:43:03 +0400

    Intial commit from previous version of the utility.

---
+++ Program.cs
@@ -0,0 +1,254 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Text;
+using NetFwTypeLib;
+
+namespace NetSpamLock
+{
+    class Program
+    {
+        /// <summary>
+        /// Structure to hold GEO IP information.
+        /// </summary>
+        public class GeoInfo
+        {
+            public string IP;
+            public string CountryName;
+            public string RegionName;
+            public string City;
+        }
+
+        /// <summary>
+        /// Program entry point.
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            var doSilent = false;
+            var doList = false;
+
+            if (args.Length > 0)
+            {
+                switch (args[0])
+                {
+                    case "-s":
+                        doSilent = true;
+                        doList = false;
+                        break;
+
+                    case "-l":
+                        doList = true;
+                        doSilent = false;
+                        break;
+
+                    case "-h":
+                    case "-help":
+                        Console.WriteLine("Khaos NetSpamLock v0.1\n");
+                        Console.WriteLine("Usage: netspamlock [-s | -l ]");
+                        Console.WriteLine("\t-s\tSilently scan and block malicious connections.");
+                        Console.WriteLine("\t-l\tList all current connections sorted by connection number.");
+                        Environment.Exit(0);
+                        break;
+                }
+            }
+
+            if (doSilent)
+            {
+                // Do silent scan and block
+                var ips = GetRemoteAddresses();
+                var connectionThreshold = uint.Parse(ConfigurationManager.AppSettings["ConnectionNumberThreshold"]);
+                var maliciousIPs =
+                    new HashSet<IPAddress>(ips.Where(x => x.Value >= connectionThreshold).Select(x => x.Key));
+
+                // Log blocked addresses
+                File.AppendAllLines("Blocked.log",
+                                    maliciousIPs.Select(
+                                        x => String.Format("{0}\t{1}", DateTime.Now, x)));
+
+                // Block malicious IPs
+                BlockAddresses(maliciousIPs);
+            }
+            else if (doList)
+            {
+                // Print list of all active connections
+                var ips = GetRemoteAddresses().OrderByDescending(x => x.Value).ToList();
+                Console.WriteLine("List of active connections by number:");
+                ips.ForEach(x => Console.WriteLine("{0}\t{1}", x.Value, x.Key));
+                Console.WriteLine();
+            }
+            else
+            {
+                // No options specified -- enter interactive mode
+                Console.WriteLine("NetSpamLock Interactive Shell\n");
+
+                var exit = false;
+
+                while (!exit)
+                {
+                    Console.Write("netspamlock> ");
+                    var query = Console.ReadLine();
+
+                    if (String.IsNullOrWhiteSpace(query))
+                    {
+                        continue;
+                    }
+
+                    var split = query.Split(' ');
+
+                    switch (split[0].ToLower())
+                    {
+                        case "list":
+                            var ips = GetRemoteAddresses().OrderByDescending(x => x.Value).ToList();
+                            Console.WriteLine("List of active connections by number:");
+                            ips.ForEach(x => Console.WriteLine("{0}\t{1}", x.Value, x.Key));
+                            Console.WriteLine();
+                            break;
+
+                        case "help":
+                            Console.WriteLine("Interactive shell commands overview.");
+                            Console.WriteLine();
+                            Console.WriteLine("list\t\tList all active connections sorted by their count from one IP.");
+                            Console.WriteLine("isin {IP}\tFind if specified IP was blacklisted.");
+                            Console.WriteLine("unblock {IP}\tDelete specified IP from the blocklist.");
+                            Console.WriteLine("block {IP}\tAdd specified IP to the blocklist.");
+                            Console.WriteLine("geo {IP}\tSearch for GEO-IP information about specified address.");
+                            Console.WriteLine("geoblocked\tSearches GEO-IP info for all currently blocked addresses.");
+                            Console.WriteLine();
+                            break;
+
+                        case "exit":
+                        case "quit":
+                            Console.WriteLine("Bye!");
+                            Environment.Exit(0);
+                            break;
+
+                        default:
+                            Console.WriteLine("Unknown command, type 'help' for basic usage.\n");
+                            break;
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Fetches GEO-IP information about specified address.
+        /// </summary>
+        /// <param name="address">Address to look for.</param>
+        /// <returns>GEO-IP information.</returns>
+        public static GeoInfo GetGeo(IPAddress address)
+        {
+            string[] content = null;
+
+            using (var webClient = new WebClient())
+            {
+                webClient.Headers.Add("User-Agent: NetSpamLock/0.1");
+                content = webClient.DownloadString(String.Format("http://freegeoip.net/csv/{0}", address)).Split(',');
+            }
+
+            if (content.Length != 11)
+            {
+                return null;
+            }
+
+            return new GeoInfo
+            {
+                IP = content[0],
+                CountryName = content[2],
+                RegionName = content[4],
+                City = content[5]
+            };
+        }
+
+        /// <summary>
+        /// Returns addresses of all currently connected remote machines.
+        /// </summary>
+        /// <returns>Currently connected IPs with information about number of simultaneous connections.</returns>
+        public static Dictionary<IPAddress, uint> GetRemoteAddresses()
+        {
+            // Gather machine network statistic
+            var ipProperties = IPGlobalProperties.GetIPGlobalProperties();
+            var tcpConnections = ipProperties.GetActiveTcpConnections();
+
+            // Get self and non-consistent IPs
+            var selfIps = Dns.GetHostEntry(Dns.GetHostName()).AddressList.ToList();
+            selfIps.Add(IPAddress.Any);
+            selfIps.Add(IPAddress.None);
+            selfIps.Add(IPAddress.Loopback);
+            //selfIps.Add(IPAddress.Broadcast);
+            selfIps.Add(IPAddress.IPv6Any);
+            //selfIps.Add(IPAddress.IPv6None);
+            selfIps.Add(IPAddress.IPv6Loopback);
+
+            // Form a hashset of remote machines
+            var ips = new Dictionary<IPAddress, uint>();
+
+            // And filter gathered metrics
+            foreach (var connection in tcpConnections.Where(connection => !selfIps.Any(selfIp => connection.RemoteEndPoint.Address.Equals(selfIp))))
+            {
+                if (!ips.ContainsKey(connection.RemoteEndPoint.Address))
+                {
+                    ips[connection.RemoteEndPoint.Address] = 0;
+                }
+
+                ips[connection.RemoteEndPoint.Address] += 1;
+            }
+
+            return ips;
+        }
+
+        /// <summary>
+        /// Blocks a set of IP addresses through Windows firewall.
+        /// </summary>
+        /// <param name="addresses">A set of addresses.</param>
+        public static void BlockAddresses(HashSet<IPAddress> addresses)
+        {
+            if (addresses == null || !addresses.Any())
+            {
+                return;
+            }
+
+            var firewallPolicy =
+                (INetFwPolicy2)Activator.CreateInstance(Type.GetTypeFromProgID("HNetCfg.FwPolicy2"));
+            var addressesStrings = new HashSet<string>(addresses.Select(x => x.ToString()));
+
+            try
+            {
+                // Find existing rule and modify it
+                var firewallRule = firewallPolicy.Rules.Item("NetSpamLock Bans");
+
+                // Accumulate all prevoius rules in hashset
+                foreach (var address in firewallRule.RemoteAddresses.Split(','))
+                {
+                    addressesStrings.Add(address);
+                }
+
+                // Restore settings if it was messed
+                firewallRule.Action = NET_FW_ACTION_.NET_FW_ACTION_BLOCK;
+                firewallRule.Direction = NET_FW_RULE_DIRECTION_.NET_FW_RULE_DIR_IN;
+                firewallRule.Enabled = true;
+                firewallRule.InterfaceTypes = "All";
+
+                // Set all rules back
+                firewallRule.RemoteAddresses = String.Join(",", addressesStrings);
+            }
+            catch (FileNotFoundException)
+            {
+                // Create a rule if it does not exists
+                var firewallRule = (INetFwRule)Activator.CreateInstance(Type.GetTypeFromProgID("HNetCfg.FwRule"));
+                firewallRule.Name = "NetSpamLock Bans";
+                firewallRule.Description = "Rule added by NetSpamLock to ban malicious IPs.";
+                firewallRule.Action = NET_FW_ACTION_.NET_FW_ACTION_BLOCK;
+                firewallRule.Direction = NET_FW_RULE_DIRECTION_.NET_FW_RULE_DIR_IN;
+                firewallRule.Enabled = true;
+                firewallRule.InterfaceTypes = "All";
+                firewallRule.RemoteAddresses = String.Join(",", addressesStrings);
+                firewallPolicy.Rules.Add(firewallRule);
+            }
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("NetSpamLock")]
+[assembly: AssemblyDescription("A tiny and helpful network utility aimed to help block malicious connection-spam attempts (i.e. DoS attacks).")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Егор 'khaos' Зеленский")]
+[assembly: AssemblyProduct("NetSpamLock")]
+[assembly: AssemblyCopyright("Copyright © Егор 'khaos' Зеленский, 2013-2014.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("52dce92f-b5d2-4674-a758-64aab153614e")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("2.0.*")]
